8b19f13dde1d96f5a3f180c236a0365d4b38a0d8
Билол <126669638+bilol-anvarov@users.noreply.github.com>
2024-02-07 22:16:55 +0500

    1.0.2

--- src/components/slider-text.js
+++ src/components/slider-text.js
@@ -1,30 +1,30 @@
 const products = [{
-        imgSrc: './src/assets/product1.jpg',
+        imgSrc: './product1.jpg',
         imgAlt: 'product 1',
         name: 'cls w218',
         price: '80 000$'
     },{
-        imgSrc: './src/assets/product2.jpg',
+        imgSrc: './product2.jpg',
         imgAlt: 'product 2',
         name: 'E63 w211',
         price: '40 000$'
     },{
-        imgSrc: './src/assets/product3.jpg',
+        imgSrc: './product3.jpg',
         imgAlt: 'product 3',
         name: 'M5 E60',
         price: '45 000$'
     },{
-        imgSrc: './src/assets/product4.webp',
+        imgSrc: './product4.webp',
         imgAlt: 'product 4',
         name: 'M5 F90',
         price: '150 000$'
     },{
-        imgSrc: './src/assets/product5.jpeg',
+        imgSrc: './product5.jpeg',
         imgAlt: 'product 5',
         name: 'Koenigsegg Agera',
         price: '3 000 000$'
     },{
-        imgSrc: './src/assets/product6.jpg',
+        imgSrc: './product6.jpg',
         imgAlt: 'product 6',
         name: 'G-Wagon',
         price: '100 000$'
